4820dcb14cbf8539352a3d8cecc92c3dcf9e3b52
Lucine Shahgholian <luc.golian@gmail.com>
2019-01-21 15:52:41 +0100

    welcome player

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Welcome to our Nim game! Please enter your player names: ");
+        Scanner scanner= new Scanner(System.in);
+        String player1=scanner.nextLine();
+
+        System.out.println("Thank you "+ player1 +". Player 2, please enter your name: ");
+        String player2= scanner.nextLine();
+
+
+
+
+
     }
 }
